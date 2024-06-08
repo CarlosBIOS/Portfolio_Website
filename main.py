9b4d2d80e960f8b365c531f2b826edf04914788b
Carlos Monteiro <cmmonteiro40@gmail.com>
2024-06-08 11:45:11 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import streamlit as st
 from os import path
 from PIL import Image
+import pandas
 
 st.set_page_config(layout='wide')  # Dá para usar no telemóvel
 st.title('Home')
 img = Image.open('Images/1.png')
 st.image(img)
+
+data = pandas.read_csv('data.csv')
+print(data)
